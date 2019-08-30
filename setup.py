a196d111d5ee8c37fa850091ff3d0feadfa9d637
Andrey Maslov <greyzmeem@gmail.com>
2019-08-30 14:48:24 +0300

    Version 0.2.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-logging-loki",
-    version="0.1.0",
+    version="0.2.0",
     description="Python logging handler for Grafana Loki.",
     long_description=long_description,
     long_description_content_type="text/markdown",

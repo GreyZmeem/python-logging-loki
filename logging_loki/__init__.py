1dc706a11de14e0cc0122d9489d7b69e91b3c6be
Andrey Maslov <greyzmeem@gmail.com>
2019-11-29 00:31:54 +0200

    Version 0.3.1

--- logging_loki/__init__.py
+++ logging_loki/__init__.py
@@ -4,5 +4,5 @@ from logging_loki.handlers import LokiHandler
 from logging_loki.handlers import LokiQueueHandler
 
 __all__ = ["LokiHandler", "LokiQueueHandler"]
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 name = "logging_loki"

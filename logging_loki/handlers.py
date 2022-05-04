ebb812fab7478063959a5ab4abc0cff365cc8f8a
Diogo <somnium@riseup.net>
2022-05-04 17:21:14 +0100

    Remove emitter version check

--- logging_loki/handlers.py
+++ logging_loki/handlers.py
@@ -49,19 +49,6 @@ class LokiHandler(logging.Handler):
 
         """
         super().__init__()
-
-        if version is None and const.emitter_ver == "0":
-            msg = (
-                "Loki /api/prom/push endpoint is in the depreciation process starting from version 0.4.0.",
-                "Explicitly set the emitter version to '0' if you want to use the old endpoint.",
-                "Or specify '1' if you have Loki version> = 0.4.0.",
-                "When the old API is removed from Loki, the handler will use the new version by default.",
-            )
-            warnings.warn(" ".join(msg), DeprecationWarning)
-
-        version = version or const.emitter_ver
-        if version not in self.emitters:
-            raise ValueError("Unknown emitter version: {0}".format(version))
         self.emitter = emitter(url, tags, auth)
 
     def handleError(self, record):  # noqa: N802
